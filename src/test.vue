60c3541449e20080bda78224e8b42f3f0a2410e2
smartn <smartn@localhost.localdomain>
2021-03-18 10:39:08 +0530

    Added Later

---
+++ src/test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class = "main">
+        <main>
+            <div class = "date">12 September 2020</div>
+        </main>
+    </div>
+</template>
+<script>
+export default {
+    name:'test',
+    date:String
+}
+</script>
+<style>
+
+</style>
